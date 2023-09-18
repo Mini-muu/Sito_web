e251d1a5d1946a84bb6520bc752ce67ce687abe0
Lorenzo Fiori <127093378+LoryFr@users.noreply.github.com>
2023-09-18 10:49:34 +0200

    Add files via upload

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,19 +1,10 @@
 function nav_dropdown_toggle() {
-	document
-		.getElementById("dropdown-menu")
-		.classList.toggle("dropdown-menu-show");
-	document.getElementById("nav-button").classList.toggle("nav-button-active");
-	var dropdown = document.getElementById("dropdown-menu");
-	dropdown.style.opacity = 0;
-	setTimeout(() => {
-		dropdown.style.opacity = 1;
-	}, this.animationDelay + 200);
-	document.getElementById("content").classList.toggle("content-active");
-}
-function autoResize() {
-	const textarea = document.getElementById("myTextarea");
-	textarea.style.height = "auto"; // Reimposta l'altezza a "auto" per evitare che il testo venga tagliato
-
-	// Imposta l'altezza del tag textarea in base al suo contenuto scrollHeight
-	textarea.style.height = textarea.scrollHeight + "px";
-}
+    document.getElementById("dropdown-menu").classList.toggle("dropdown-menu-show");
+    document.getElementById("nav-button").classList.toggle("nav-button-active")
+    var dropdown = document.getElementById("dropdown-menu");
+    dropdown.style.opacity = 0;
+    setTimeout(() => {
+    dropdown.style.opacity = 1;
+    }, this.animationDelay + 200);
+    document.getElementById("content").classList.toggle("content-active");
+  }
